42e7a55013f4505f5f69fab2789be15961e265ef
antigone2 <antigone2@users.noreply.github.com>
2017-06-20 20:03:30 +0200

    worker update

--- js/worker.js
+++ js/worker.js
@@ -2,31 +2,29 @@
 let count = 0, id, id2;
 /*constatnts*/
 const cache = new Set(), CO = 'Count', CON = 'Established cpnnections';
-  
-  function start() {
-     id = setInterval(function() {
-     count++;
-     for (port of cache) {
-      port.postMessage({mess1: [CO,  count], mess2: [CON,cache.size], mess3: new Date().toLocaleString()});
-     }
-     }, 500);
-  } 
-  
-  start ();
 
-onconnect = function(e) {
-  let port = e.ports[0];
-  cache.add(port);
-  port.start();  
+function start() {
+   id = setInterval(function () {
+      count++;
+      for (port of cache) {
+         port.postMessage({mess1: [CO, count], mess2: [CON, cache.size], mess3: new Date().toLocaleString()});
+      }
+   }, 500);
+}
 
-  port.onmessage = function (event) {
+start(); 
+
+onconnect = function (e) {
+   let port = e.ports[0];
+   cache.add(port);
+   port.start();
+
+   port.onmessage = function (event) {
       clearInterval(id);
       count = 0;
-     
-   if (event.data == 'stop') {
-   }
-   if (event.data == 'start') {
-      start();
-   }
-  };
+
+      if (event.data == 'start') {
+         start();
+      }
+   };
 }
